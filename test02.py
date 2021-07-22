317d07df4f323dc094ab9c49e8cbc96a799ff23f
Yassy <yasuhito.mori@microfocus.com>
2021-07-22 19:43:14 +0900

    202107221943

--- test02.py
+++ test02.py
@@ -10,9 +10,10 @@ create table users (
 """
 db.execute(sql)
 for num in range(1000):
-    sql = "insert into users values (num, 'foo', 'bar')"
+    sql = "insert into users values (" + str(num) + ", 'foo', 'bar')"
     db.execute(sql)
 c = db.cursor()
 c.execute("select * from users where id = 1")
 for row in c:
-    print(row) 
+    if row > 995:
+        print(row) 
