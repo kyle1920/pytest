957e67f3e759c26217937eaaec88189c2ac4d1be
Yassy <yasuhito.mori@microfocus.com>
2021-07-22 20:02:00 +0900

    202107222001

--- test02.py
+++ test02.py
@@ -12,10 +12,11 @@ db.execute(sql)
 #for num in range(1000):
 for num in range(5):
     print(str(num))
-    sql = "insert into users values (" + str(num) + ", 'foo', 'bar')"
+    sql = "insert into users values (" + str(num + 1) + ", 'foo', 'bar')"
     db.execute(sql)
 c = db.cursor()
-c.execute("select * from users where id = 1")
+#c.execute("select * from users where id = 1")
+c.execute("select * from users")
 #idx = 0
 for row in c:
  #   idx +=1
