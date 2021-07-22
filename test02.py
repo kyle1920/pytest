bf78c967dcd40f1a56b61855ff5a48998420586d
Yassy <yasuhito.mori@microfocus.com>
2021-07-22 19:26:25 +0900

    202107221926

---
+++ test02.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+db = sqlite3.connect(":memory:")
+sql = """
+create table users (
+  id integer,
+  name varchar(10),
+  age integer
+);
+"""
+db.execute(sql)
+for num in range(1000):
+    sql = "insert into users values (num, 'foo', 'bar')"
+    db.execute(sql)
+c = db.cursor()
+c.execute("select * from users where id = 1")
+for row in c:
+    print(row) 
