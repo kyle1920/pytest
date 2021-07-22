840788859c10664123e7a9533fc4ca581f7ea655
Yassy <yasuhito.mori@microfocus.com>
2021-07-22 20:16:36 +0900

    202107222016

--- test02.py
+++ test02.py
@@ -9,16 +9,19 @@ create table users (
 );
 """
 db.execute(sql)
-for num in range(1000):
+for num in range(10000):
 #for num in range(5):
     print(str(num))
     sql = "insert into users values (" + str(num + 1) + ", 'foo', 'bar')"
     db.execute(sql)
 c = db.cursor()
 #c.execute("select * from users where id = 1")
-c.execute("select * from users")
-idx = 0
+#c.execute("select * from users")
+c.execute("select sum(id) from users")
+#idx = 0
+#for row in c:
+#   idx +=1
+#   if idx > 994:
+#        print(row)
 for row in c:
-   idx +=1
-   if idx > 994:
-        print(row)
+   print(row)
