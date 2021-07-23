2a1562e3d39819a13fa9aab30321052d78b04911
Yassy <yasuhito.mori@microfocus.com>
2021-07-23 20:47:02 +0900

    202107232047

--- test02.py
+++ test02.py
@@ -2,29 +2,36 @@ import sqlite3
 from ddtrace import Pin, patch
 patch(sqlite3=True)
 
-db = sqlite3.connect(":memory:")
-sql = """
-create table users (
-id integer,
-name varchar(10),
-age integer
-);
-"""
-db.execute(sql)
+def createTable():
 
-for num in range(10000):
-#for num in range(5):
-#    print(str(num))
-    sql = "insert into users values (" + str(num + 1) + ", 'foo', 'bar')"
+    db = sqlite3.connect(":memory:")
+    sql = """
+    create table users (
+    id integer,
+    name varchar(10),
+    age integer
+    );
+    """
     db.execute(sql)
-c = db.cursor()
-#c.execute("select * from users where id = 1")
-c.execute("select * from users")
-#c.execute("select sum(id) from users")
-idx = 0
-for row in c:
-    idx +=1
-    if idx > 9994:
-        print(row)
-#for row in c:
-#    print(row)
+
+def readTable():
+    for num in range(10000):
+    #for num in range(5):
+    #    print(str(num))
+        sql = "insert into users values (" + str(num + 1) + ", 'foo', 'bar')"
+        db.execute(sql)
+    c = db.cursor()
+    #c.execute("select * from users where id = 1")
+    c.execute("select * from users")
+    #c.execute("select sum(id) from users")
+    idx = 0
+    for row in c:
+        idx +=1
+        if idx > 9994:
+            print(row)
+    #for row in c:
+    #    print(row)
+
+    createTable()
+
+    readTable()
