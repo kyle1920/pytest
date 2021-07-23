1f75249327bb3b47af301cf6d774a78a8690fdb7
Yassy <yasuhito.mori@microfocus.com>
2021-07-23 17:32:05 +0900

    202107231732

--- hello.py
+++ hello.py
@@ -1,17 +1,13 @@
 from datadog import initialize, statsd
+from ddtrace import Pin, patch
+patch(flask=True)
 
-options = {
-    'statsd_host':'127.0.0.1',
-    'statsd_port':8125
-}
+#options = {
+#    'statsd_host':'127.0.0.1',
+#    'statsd_port':8125
+#}
 
-initialize(**options)
-
-statsd.service_check(
-    check_name="application.service_check",
-    status="0",
-    message="Application is OK",
-)
+#initialize(**options)
 
 # 必要なモジュールの取り込み
 from flask import Flask
@@ -23,6 +19,17 @@ app = Flask(__name__)
 @app.route("/")
 def root():
     return "Hello"
+
+    #   tagと値を作る
+    #tags = ['version:1', 'application:web']
+    #value=random.randint(0, 100)
+
+    #   metricのset
+    #metric="myapp.testdata.set"
+    #statsd.set(metric, value, tags=tags)
+    #   metricのgauge
+    #metric="myapp.testdata.gauge"
+    #statsd.gauge(metric, value, tags=tags)
 
 # サーバーを起動 --- (*3)
 if __name__ == "__main__":
