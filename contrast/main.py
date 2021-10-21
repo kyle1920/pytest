8ecd9be055215e72c1dcb14128270d7042447c23
Yassy <yasuhito.mori@microfocus.com>
2021-10-21 22:44:42 +0900

    2021/10/21 22:44 Vlun code inserted

--- contrast/main.py
+++ contrast/main.py
@@ -17,6 +17,9 @@ def post():
 		title = 'Form Sample(post)', \
 		message = 'こんにちは、{}さん'.format(name))
 
+@app.route('/echo', methods=['GET'])
+def echo():
+    return render_template_string(request.args.get('q', ''))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=8888, threaded=True)
