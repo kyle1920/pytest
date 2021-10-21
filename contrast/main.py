7b05d2f7e7c1c9b252116b101ac830f34fb16dec
Yassy <yasuhito.mori@microfocus.com>
2021-10-21 22:27:12 +0900

    21-10-21 22:27

--- contrast/main.py
+++ contrast/main.py
@@ -1,19 +1,21 @@
-from flask import *
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# getのときの処理
+@app.route('/', methods=['GET'])
+def get():
+	return render_template('index.html', \
+		title = 'Form Sample(get)', \
+		message = '名前を入力して下さい。')
 
 # postのときの処理	
 @app.route('/', methods=['POST'])
 def post():
-	memo = request.form['memo']
-	return render_template('index.html',
-    message = '入力された内容は{}です'.format(memo))
-
-
-@app.route("/<name>", methods=["GET", "POST"])
-def namepage(name):
-    return render_template("name.html")
+	name = request.form['name']
+	return render_template('index.html', \
+		title = 'Form Sample(post)', \
+		message = 'こんにちは、{}さん'.format(name))
 
 
 if __name__ == "__main__":
